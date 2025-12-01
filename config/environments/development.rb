8caa2d3a67e2a2749ce62a63073a567acd4ba119
Bailly Hugo <gucci60610@gmail.com>
2025-12-01 15:41:08 +0100

    Deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,11 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.default_url_options = { host: 'MEQUI-AI.herokuapp.com' }
+
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  post "/quick_matching", to: "matches#quick_create"
+  post "/quick_matching", to: "matches#quick_create"
+  get "/my_matches", to: "matches#index"
+  post "/matching", to: "matches#create"
+  resource :profile, only: [:edit, :update]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
