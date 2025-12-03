da2ac911855c6973a4e7dc8e66b4c4c0b09e9fe3
Bailly Hugo <gucci60610@gmail.com>
2025-12-03 15:00:14 +0100

    deleting stashed upstreams lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< Updated upstream
 
   root to: "pages#home"
 
@@ -10,26 +9,23 @@ Rails.application.routes.draw do
 
   post "/quick_matching", to: "matches#quick_create"
   get  "/my_matches", to: "matches#index"
-=======
+
 
   root to: "pages#home"
 
   # Matches existants (j'ai viré le doublon quick_matching)
   post "/quick_matching", to: "matches#quick_create"
   get "/my_matches", to: "matches#index"
->>>>>>> Stashed changes
   post "/matching", to: "matches#create"
 
   resource :profile, only: [:edit, :update]
 
-<<<<<<< Updated upstream
-=======
+
   # Chat IA – un seul chat par user, messages nested
   resource :chat, only: [:show] do
     resources :messages, only: [:create]
   end
 
   # Health check
->>>>>>> Stashed changes
   get "up" => "rails/health#show", as: :rails_health_check
 end
