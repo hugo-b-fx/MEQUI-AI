dfe1cc44fe4e8ef7e7bad80005958c93dc23fc3c
Bailly Hugo <gucci60610@gmail.com>
2025-12-03 16:29:41 +0100

    still figuring out the issue on the view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :fake_user_for_dev
-
-  def fake_user_for_dev
-    return if current_user.present?
-
-    @dev_user ||= User.first || User.create!(
-      email: "dev@example.com",
-      password: "password123",
-      name: "Dev User",
-      role: :rider
-    )
-
-    sign_in(@dev_user)
-  end
-
 end
--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,5 @@
 class ChatsController < ApplicationController
-
-  before_action :authenticate_user!
+  before_action :ensure_user
 
   def show
     @chat = current_user.chat || current_user.create_chat!
@@ -14,5 +13,24 @@ class ChatsController < ApplicationController
 
     @message  = Message.new
     @messages = @chat.messages.order(:created_at).last(15)
+  end
+
+  private
+
+  def ensure_user
+    return if current_user.present?
+
+    temp_user = User.find_by(email: "dev@example.com")
+
+    unless temp_user
+      temp_user = User.create!(
+        email: "dev@example.com",
+        password: "password123",
+        name: "Dev User",
+        role: :rider
+      )
+    end
+
+    sign_in(temp_user)
   end
 end
