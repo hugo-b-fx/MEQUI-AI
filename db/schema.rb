ab67647a8803ee1d5bfcfe21dfc610108b914e9a
marie miribel <marie.miribel@audencia.com>
2025-12-04 15:49:49 +0100

    Add name and role to sign up form and update home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_02_163544) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_03_103823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_02_163544) do
     t.string "breed"
     t.integer "age"
     t.string "discipline"
-    t.text "photos"
+    t.string "photos"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_horses_on_user_id"
