2ce13336c105c9645b003425b6efd2cf3280f1c8
Bailly Hugo <gucci60610@gmail.com>
2025-12-05 16:14:44 +0100

    revert last push

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "pages#home"
+  root to: "pages#home"
 
-  resources :chats, only: [:show] do
+  post "/quick_matching", to: "matches#quick_create"
+  get  "/my_matches",     to: "matches#index"
+  post "/matching",       to: "matches#create"
+
+  resource :profile, only: [:edit, :update]
+
+  resource :chat, only: [:show] do
     resources :messages, only: [:create]
   end
 
-  get "/chat", to: "chats#show"
+  get "up" => "rails/health#show", as: :rails_health_check
 end
